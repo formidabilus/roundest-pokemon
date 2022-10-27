46a74a7dfbe54d014e841d4b0b6c0ad3ebe69d0c
formidabilus <chiriac_razvan@ymail.com>
2022-10-27 22:50:26 +0300

    basic UI structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,17 @@ import { trpc } from "../utils/trpc";
 const Home: NextPage = () => {
   const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
 
-  return <div className="">Hello world</div>;
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="text-center text-2xl">Which Pokémon is Rounder?</div>
+      <div className="p-2"></div>
+      <div className="flex max-w-2xl items-center justify-between rounded border p-8 ">
+        <div className=" h-16 w-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="h-16 w-16 bg-red-200" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
